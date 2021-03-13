1ab1a402fe8343e23da6ecf258e9015f23c03a4e
asao@skillengine.jp <k.tessyma1120@gmail.com>
2021-03-13 13:01:42 +0900

    タイトル・ホーム作成

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>Sample Blog</h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            <h1><?php bloginfo('name'); ?></h1>
+            <span class="subheading"><?php bloginfo('description'); ?></span>
           </div>
         </div>
       </div>
