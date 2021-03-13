d4159625f8bddaf80dde8b34c448c3863d4aa7c4
asao@skillengine.jp <k.tessyma1120@gmail.com>
2021-03-13 12:10:45 +0900

    ページネーション

--- index.php
+++ index.php
@@ -29,27 +29,40 @@
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <?php if (have_posts()) : ?>
-          <?php while(have_posts()): the_post(); ?>
-          <div class="post-preview">
-            <a href="<?php the_permalink(); ?>">
-              <h2 class="post-title">
-                <?php the_title(); ?>
-              </h2>
-              <h3 class="post-subtitle">
-                <!-- 投稿の際には抜粋の入力を行う -->
-                <?php the_excerpt(); ?>
-              </h3>
-            </a>
-            <p class="post-meta">Posted by
-              <?php the_author()?>
+          <?php while (have_posts()) : the_post(); ?>
+            <div class="post-preview">
+              <a href="<?php the_permalink(); ?>">
+                <h2 class="post-title">
+                  <?php the_title(); ?>
+                </h2>
+                <h3 class="post-subtitle">
+                  <!-- 投稿の際には抜粋の入力を行う -->
+                  <?php the_excerpt(); ?>
+                </h3>
+              </a>
+              <p class="post-meta">Posted by
+                <?php the_author() ?>
                 <?php the_time(get_option('date_format')); ?>
-            </p>
-          </div>
-          <hr>
+              </p>
+            </div>
+            <hr>
           <?php endwhile; ?>
           <!-- Pager -->
           <div class="clearfix">
-            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+            <?php
+            $link = get_previous_posts_link('&larr; 新しい記事へ');
+            if ($link) {
+              $link = str_replace('<a', '<a class="btn btn-primary float-left"', $link);
+              echo $link;
+            }
+            ?>
+            <?php
+            $link = get_next_posts_link('古い記事へ &rarr;');
+            if ($link) {
+              $link = str_replace('<a', '<a class="btn btn-primary float-right"', $link);
+              echo $link;
+            }
+            ?>
           </div>
         <?php else : ?>
           <p>OMG！記事が見つかりませんでした</p>
